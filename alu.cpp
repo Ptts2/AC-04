a94e57c14ddfda955ea422e7d2725d6c27645065
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2020-04-09 12:38:01 +0200

    Añadidos dos métodos para convertir de decimal a IEEE y viceversa

--- alu.cpp
+++ alu.cpp
@@ -24,3 +24,15 @@ float Alu::division()
 
     return 0.0;
 }
+
+Alu::IEEE754num Alu::fromDecToIEEE754(float num){
+
+    IEEE754num numeroConvertido;
+
+    return numeroConvertido;
+}
+
+float Alu::fromIEEE754ToDec(Alu::IEEE754num){
+
+    return 0.0;
+}
--- alu.h
+++ alu.h
@@ -5,11 +5,19 @@
 class Alu
 {
 public:
+    struct IEEE754num{
+        int mantisa : 23;
+        int exponente : 8;
+        int signo: 1;
+
+    };
     Alu();
     ~Alu();
     float suma(/*Ver que incluir*/);
     float producto(/*Ver que incluir*/);
     float division(/*Ver que incluir*/);
+    IEEE754num fromDecToIEEE754(float num);
+    float fromIEEE754ToDec(IEEE754num);
 
 };
 
